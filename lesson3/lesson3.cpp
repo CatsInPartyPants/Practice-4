23bdd1e9297cdc166d1ae77f402199e6ac3b6ea0
Artem Nilov <nilov1235@mail.ru>
2022-05-08 15:39:03 +0300

    Добавьте файлы проекта.

---
+++ lesson3/lesson3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a = 54, b = 100, c= 10, d = 10;
+    if (a > b) {
+        std::cout << a;
+    }
+    else if (a == b && c == d) {
+        std::cout << a + b;
+    }
+    else {
+        std::cout << b;
+    }
+}
+
+
